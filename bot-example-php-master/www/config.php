3da737aa3cbe4aec9550bf34e550d4935d67af09
pavelsmagulov <48508481+pavelsmagulov@users.noreply.github.com>
2019-03-13 11:28:43 +0400

    Add files via upload

---
+++ bot-example-php-master/www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '45ae2b2f'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '07d4651a390dad2bbf329f89f83d2349084c694bcf1cd6daa3136128b960f18531e361595c768c670ae37f'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
